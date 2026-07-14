3ade62260e546f99f01b3e92ce19aeb5cd23cb73
Kleiton Sobral <kleiton.c@aluno.senai.br>
2026-07-14 16:38:26 -0300

    Refactor variable declarations for weight and height
    Refactor variable declarations and improve code readability.

--- Lista de Exercícios - Condição e Repetição/LISTA_EXERCICIOS_Q6/src/main/java/com/mycompany/lista_exercicios_q6/LISTA_EXERCICIOS_Q6.java
+++ Lista de Exercícios - Condição e Repetição/LISTA_EXERCICIOS_Q6/src/main/java/com/mycompany/lista_exercicios_q6/LISTA_EXERCICIOS_Q6.java
@@ -1,3 +1,14 @@
+/*
+Crie um programa em Java que solicite o peso, em quilogramas, e a altura, em metros, de uma pessoa.
+Em seguida, calcule o Índice de Massa Corporal (IMC) utilizando a fórmula:
+IMC = peso ÷ altura²
+Após o cálculo, informe a classificação correspondente:
+* Abaixo do peso;
+* Peso normal;
+* Excesso de peso;
+* Obesidade.
+*/
+
 package com.mycompany.lista_exercicios_q6;
 
 import java.util.Scanner;
@@ -5,20 +16,19 @@ import java.util.Scanner;
 public class LISTA_EXERCICIOS_Q6 {
 
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
-        
-        double peso = 0;
-        double altura = 0;
-        double imc = 0;
-        
+
+        double peso = 0, imc = 0, altura = 0;
+
         System.out.println("Digite o seu peso em KG:");
         peso = scanner.nextDouble();
-        
+
         System.out.println("Digite a sua altuta em METROS:");
         altura = scanner.nextDouble();
-        
+
         imc = peso / (altura*altura);
-        
+
         if( imc < 18.8 ){
             System.out.println("ABAIXO DO PESO");
         }
@@ -31,9 +41,7 @@ public class LISTA_EXERCICIOS_Q6 {
         else if ( imc >=30 && imc <=34.9){
             System.out.print("OBESIDADE");
         }
-        
-        
+
         scanner.close();
-             
     }
 }
