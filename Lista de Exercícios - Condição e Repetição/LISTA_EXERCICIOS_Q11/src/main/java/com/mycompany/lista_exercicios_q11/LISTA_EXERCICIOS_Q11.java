b44d8a1806b8aa7d55d8d668f1c1bae7e63570b4
Kleiton Sobral <kleiton.c@aluno.senai.br>
2026-07-14 17:05:46 -0300

    Implement temperature conversion program in Java
    Added a Java program to convert temperatures between Celsius and Fahrenheit based on user input.

--- Lista de Exercícios - Condição e Repetição/LISTA_EXERCICIOS_Q11/src/main/java/com/mycompany/lista_exercicios_q11/LISTA_EXERCICIOS_Q11.java
+++ Lista de Exercícios - Condição e Repetição/LISTA_EXERCICIOS_Q11/src/main/java/com/mycompany/lista_exercicios_q11/LISTA_EXERCICIOS_Q11.java
@@ -1,3 +1,15 @@
+/*
+Crie um programa em Java que solicite ao usuário uma temperatura e a unidade de origem, 
+sendo `C` para Celsius ou `F` para Fahrenheit.
+O programa deverá:
+* Converter de Celsius para Fahrenheit quando a unidade informada for `C`;
+* Converter de Fahrenheit para Celsius quando a unidade informada for `F`;
+* Exibir uma mensagem de erro caso seja informada uma unidade inválida.
+Utilize as seguintes fórmulas:
+Fahrenheit = (Celsius × 9 ÷ 5) + 32
+Celsius = (Fahrenheit − 32) × 5 ÷ 9
+*/
+
 package com.mycompany.lista_exercicios_q11;
 
 import java.util.Scanner;
@@ -5,32 +17,29 @@ import java.util.Scanner;
 public class LISTA_EXERCICIOS_Q11 {
 
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
 
-        
         System.out.println("Digite a temperatura que deseja converter:");
         double temperatura = scanner.nextDouble();
 
-        
         System.out.println("Digite a unidade de origem (C para Celsius, F para Fahrenheit):");
         char unidadeOrigem = scanner.next().toUpperCase().charAt(0);
 
         double temperaturaConvertida;
 
-        
         if (unidadeOrigem == 'C') {
-            
+
             temperaturaConvertida = (temperatura * 9/5) + 32;
             System.out.println(temperatura + " C e igual a " + temperaturaConvertida + " F");
         } else if (unidadeOrigem == 'F') {
-            
+
             temperaturaConvertida = (temperatura - 32) * 5/9;
             System.out.println(temperatura + "F e igual a " + temperaturaConvertida + "C");
         } else {
-            
+
             System.out.println("Unidade de origem invalida. Use 'C' para Celsius ou 'F' para Fahrenheit.");
         }
-
         scanner.close();
     }
 }
