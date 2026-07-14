1fd14927626399b604f0613f8edbf9f3eefbeaaf
Kleiton Sobral <kleiton.c@aluno.senai.br>
2026-07-14 17:34:14 -0300

    Add comments and clean up code in main method
    Added comments to explain the program's functionality.

--- Lista de Exercícios - Condição e Repetição/LISTA_EXERCICIOS_Q19/src/main/java/com/mycompany/lista_exercicios_q19/LISTA_EXERCICIOS_Q19.java
+++ Lista de Exercícios - Condição e Repetição/LISTA_EXERCICIOS_Q19/src/main/java/com/mycompany/lista_exercicios_q19/LISTA_EXERCICIOS_Q19.java
@@ -1,3 +1,9 @@
+/*
+Crie um programa em Java que solicite ao usuário dois números inteiros: uma base e um expoente.
+Em seguida, o programa deverá calcular a potência da base elevada ao expoente 
+utilizando uma estrutura de repetição, sem usar métodos prontos, e exibir o resultado na tela.
+*/
+
 package com.mycompany.lista_exercicios_q19;
 
 import java.util.Scanner;
@@ -5,24 +11,20 @@ import java.util.Scanner;
 public class LISTA_EXERCICIOS_Q19 {
 
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
 
-        
         System.out.println("Digite a base:");
         int base = scanner.nextInt();
 
         System.out.println("Digite o expoente:");
         int expoente = scanner.nextInt();
 
-        
         int resultado = 1;
 
-        
         for (int i = 1; i <= expoente; i++) {
-            resultado *= base; 
+            resultado *= base;
         }
-
-        
         System.out.println(base + "^" + expoente + " = " + resultado);
 
         scanner.close();
