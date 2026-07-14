e03050077fe33c56d8ea7ab344f4d0984f34a5e3
Kleiton Sobral <kleiton.c@aluno.senai.br>
2026-07-14 17:00:36 -0300

    Add comment for countdown program implementation
    Add a comment block describing a countdown program.

--- Lista de Exercícios - Condição e Repetição/LISTA_EXERCICIOS_Q10/src/main/java/com/mycompany/lista_exercicios_q10/LISTA_EXERCICIOS_Q10.java
+++ Lista de Exercícios - Condição e Repetição/LISTA_EXERCICIOS_Q10/src/main/java/com/mycompany/lista_exercicios_q10/LISTA_EXERCICIOS_Q10.java
@@ -1,3 +1,9 @@
+/*
+Crie um programa em Java que realize uma contagem regressiva de 10 até 0 
+utilizando a estrutura de repetição `while`.
+Ao final da contagem, o programa deverá exibir a mensagem “Contagem regressiva concluída!”.
+*/
+
 package com.mycompany.lista_exercicios_q10;
 
 public class LISTA_EXERCICIOS_Q10 {
