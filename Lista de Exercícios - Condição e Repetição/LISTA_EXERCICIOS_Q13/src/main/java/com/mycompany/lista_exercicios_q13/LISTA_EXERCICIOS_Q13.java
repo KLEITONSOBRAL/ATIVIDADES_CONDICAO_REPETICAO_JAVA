1871ef3f806e4dd35cf486af3c74acd84c2afbd5
Kleiton Sobral <kleiton.c@aluno.senai.br>
2026-07-14 17:15:56 -0300

    Add prompt for user input in main method

--- Lista de Exercícios - Condição e Repetição/LISTA_EXERCICIOS_Q13/src/main/java/com/mycompany/lista_exercicios_q13/LISTA_EXERCICIOS_Q13.java
+++ Lista de Exercícios - Condição e Repetição/LISTA_EXERCICIOS_Q13/src/main/java/com/mycompany/lista_exercicios_q13/LISTA_EXERCICIOS_Q13.java
@@ -13,7 +13,7 @@ import java.util.Scanner;
 public class LISTA_EXERCICIOS_Q13 {
 
     public static void main(String[] args) {
-        
+
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Digite um ano para verificar se é bissexto:");
