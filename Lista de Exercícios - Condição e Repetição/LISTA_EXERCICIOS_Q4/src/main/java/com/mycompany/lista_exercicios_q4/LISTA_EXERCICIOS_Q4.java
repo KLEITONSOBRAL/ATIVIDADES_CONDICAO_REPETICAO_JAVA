3738613c0b104bbd3579598a96141ad53d9c1efc
Kleiton Sobral <kleiton.c@aluno.senai.br>
2026-07-14 16:26:45 -0300

    Fix typo in access block message

--- Lista de Exercícios - Condição e Repetição/LISTA_EXERCICIOS_Q4/src/main/java/com/mycompany/lista_exercicios_q4/LISTA_EXERCICIOS_Q4.java
+++ Lista de Exercícios - Condição e Repetição/LISTA_EXERCICIOS_Q4/src/main/java/com/mycompany/lista_exercicios_q4/LISTA_EXERCICIOS_Q4.java
@@ -1,3 +1,11 @@
+/*
+Crie um programa em Java que simule um sistema de login.
+O programa deverá possuir um nome de usuário e uma senha previamente cadastrados. 
+Em seguida, deverá solicitar que o usuário informe suas credenciais.
+O sistema deverá permitir no máximo três tentativas de acesso. 
+Caso o usuário e a senha estejam corretos, deverá exibir a mensagem “Login bem-sucedido”. Caso as três tentativas sejam utilizadas sem sucesso, deverá informar que o acesso foi bloqueado.
+*/
+
 package com.mycompany.lista_exercicios_q4;
 
 import java.util.Scanner;
@@ -5,9 +13,9 @@ import java.util.Scanner;
 public class LISTA_EXERCICIOS_Q4 {
 
     public static void main(String[] args) {
+        
         Scanner scanner = new Scanner(System.in);
-
-        // Usuário e senha predefinidos
+        
         String usuarioCorreto = "KLEITON";
         String senhaCorreta = "12345";
 
@@ -31,7 +39,7 @@ public class LISTA_EXERCICIOS_Q4 {
         }
 
         if (!loginBemSucedido) {
-            System.out.println("Numero maximo de tentativas excedido. Acesso bloqueado.");
+            System.out.println("Número máximo de tentativas excedido. Acesso bloqueado.");
         }
 
         scanner.close();
