d8fe0af250ff9e40699921832037158902bfcb94
Kleiton Sobral <kleiton.c@aluno.senai.br>
2026-07-14 16:44:39 -0300

    Fix output messages and comments for clarity

--- Lista de Exercícios - Condição e Repetição/LISTA_EXERCICIOS_Q7/src/main/java/com/mycompany/lista_exercicios_q7/LISTA_EXERCICIOS_Q7.java
+++ Lista de Exercícios - Condição e Repetição/LISTA_EXERCICIOS_Q7/src/main/java/com/mycompany/lista_exercicios_q7/LISTA_EXERCICIOS_Q7.java
@@ -1,3 +1,8 @@
+/*
+Crie um programa em Java que solicite ao usuário um número inteiro positivo. 
+Em seguida, percorra todos os números de 1 até o valor informado, conte quantos são pares e quantos são ímpares e exiba os resultados na tela.
+*/
+
 package com.mycompany.lista_exercicios_q7;
 
 import java.util.Scanner;
@@ -5,16 +10,15 @@ import java.util.Scanner;
 public class LISTA_EXERCICIOS_Q7 {
 
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
 
-        
-        System.out.println("Digite um numero:");
+        System.out.println("Digite um número:");
         int numero = scanner.nextInt();
 
         int contagemPares = 0;
         int contagemImpares = 0;
 
-        
         for (int i = 1; i <= numero; i++) {
             if (i % 2 == 0) {
                 contagemPares++;
@@ -23,9 +27,8 @@ public class LISTA_EXERCICIOS_Q7 {
             }
         }
 
-        
-        System.out.println("Quantidade de numeros pares: " + contagemPares);
-        System.out.println("Quantidade de numeros impares: " + contagemImpares);
+        System.out.println("Quantidade de números pares: " + contagemPares);
+        System.out.println("Quantidade de números impares: " + contagemImpares);
 
         scanner.close();
     }
