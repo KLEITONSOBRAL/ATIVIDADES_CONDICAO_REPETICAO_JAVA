00951ff7554f8e8c3db0f983ea4123bdeaee1cee
Kleiton Sobral <kleiton.c@aluno.senai.br>
2026-07-14 16:30:11 -0300

    Fix typo in user prompt for multiplication table

--- Lista de Exercícios - Condição e Repetição/LISTA_EXERCICIOS_Q5/src/main/java/com/mycompany/lista_exercicios_q5/LISTA_EXERCICIOS_Q5.java
+++ Lista de Exercícios - Condição e Repetição/LISTA_EXERCICIOS_Q5/src/main/java/com/mycompany/lista_exercicios_q5/LISTA_EXERCICIOS_Q5.java
@@ -1,3 +1,8 @@
+/*
+Crie um programa em Java que solicite ao usuário um número inteiro
+e exiba sua tabuada de multiplicação, de 1 até 10.
+*/
+
 package com.mycompany.lista_exercicios_q5;
 
 import java.util.Scanner;
@@ -5,17 +10,18 @@ import java.util.Scanner;
 public class LISTA_EXERCICIOS_Q5 {
 
     public static void main(String[] args) {
+        
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Digite um numero para ver a tabuada:");
+        System.out.println("Digite um número para ver a tabuada:");
         int numero = scanner.nextInt();
 
         System.out.println("Tabuada do " + numero + ":");
+        
         for (int i = 1; i <= 10; i++) {
             int resultado = numero * i;
             System.out.println(numero + " x " + i + " = " + resultado);
         }
-
         scanner.close();
     }
 }
