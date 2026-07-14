635714dd40d0d74e955516e3a9c54755ae9b6b1f
Kleiton Sobral <kleiton.c@aluno.senai.br>
2026-07-14 17:28:29 -0300

    Count vowels in user input string

--- Lista de Exercícios - Condição e Repetição/LISTA_EXERCICIOS_Q17/src/main/java/com/mycompany/lista_exercicios_q17/LISTA_EXERCICIOS_Q17.java
+++ Lista de Exercícios - Condição e Repetição/LISTA_EXERCICIOS_Q17/src/main/java/com/mycompany/lista_exercicios_q17/LISTA_EXERCICIOS_Q17.java
@@ -1,3 +1,10 @@
+/*
+Crie um programa em Java que solicite ao usuário uma frase e conte a quantidade 
+de vogais presentes nela.
+O programa deverá considerar as vogais `a`, `e`, `i`, `o` e `u`, 
+independentemente de estarem escritas com letras maiúsculas ou minúsculas. Ao final, deverá exibir a quantidade total de vogais encontradas.
+*/
+
 package com.mycompany.lista_exercicios_q17;
 
 import java.util.Scanner;
@@ -5,26 +12,23 @@ import java.util.Scanner;
 public class LISTA_EXERCICIOS_Q17 {
 
     public static void main(String[] args) {
+        
         Scanner scanner = new Scanner(System.in);
-
         
         System.out.println("Digite uma frase:");
-        String frase = scanner.nextLine().toLowerCase(); 
+        String frase = scanner.nextLine().toLowerCase();
 
         int contagemVogais = 0;
-
         
         for (int i = 0; i < frase.length(); i++) {
             char caractere = frase.charAt(i);
-
             
             if (caractere == 'a' || caractere == 'e' || caractere == 'i' || caractere == 'o' || caractere == 'u') {
                 contagemVogais++;
             }
         }
-
         
-        System.out.println("NUmero de vogais na frase: " + contagemVogais);
+        System.out.println("Número de vogais na frase: " + contagemVogais);
 
         scanner.close();
     }
